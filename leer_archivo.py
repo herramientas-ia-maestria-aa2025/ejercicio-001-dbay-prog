8a82682ebd14853ca94f0d4220927546dc7fe005
Diego <sayaboge@gmail.com>
2025-04-10 20:40:11 -0500

    Lectura de archivo

---
+++ leer_archivo.py
@@ -0,0 +1,4 @@
+f = open('holamundo.txt','r') # Abre el archivo para lectura [1]
+mensaje = f.read() # Lee el archivo y lo guarda en la variable mensaje [1]
+print(mensaje) # Imprime el mensaje en la consola [1]
+f.close() # Cierra el archivo [1]
